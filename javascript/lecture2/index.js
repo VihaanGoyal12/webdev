537d84d021433351510876ccf59503ff0dec9cf1
VihaanGoyal12 <102225589+VihaanGoyal12@users.noreply.github.com>
2026-08-16 23:51:34 +0530

    more into functions

--- javascript/lecture2/index.js
+++ javascript/lecture2/index.js
@@ -1,4 +1,17 @@
-function sayname(){
-    console.log("vihaan");
-}
-sayname();
+// function sayname(){
+//     console.log("vihaan");
+// }
+// sayname();
+// function printNum(){
+//     for(let i =0;i<100;i++){
+//         console.log(i);
+//     }
+// }
+// printNum();
+// function printNum(num){
+//     console.log(num);
+//     if(num == 0) return;
+//     printNum(num-1);
+    
+// }
+// printNum(5);

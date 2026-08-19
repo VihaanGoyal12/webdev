0ddd0b1150360123299c15762329bc12b76ecdb3
VihaanGoyal12 <102225589+VihaanGoyal12@users.noreply.github.com>
2026-08-19 23:40:11 +0530

    understanding variables

---
+++ javascript/lecture3/index.js
@@ -0,0 +1,11 @@
+var num= 22;
+console.log(num);
+//var has global scope
+
+let age = 18;
+console.log(age);
+//let has local scope
+
+const val = 30;
+console.log(val);
+//const has local scope and cant be altered later in the code

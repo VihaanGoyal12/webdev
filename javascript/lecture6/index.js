b221d9bbea7011ba47d37d64c1232e56acb34ee4
VihaanGoyal12 <102225589+VihaanGoyal12@users.noreply.github.com>
2026-08-22 23:21:32 +0530

    arrays in js

---
+++ javascript/lecture6/index.js
@@ -0,0 +1,19 @@
+let students = ['student1','student2','student3'];
+console.log(students.length);
+console.log(students[2]);
+students[1] = 'hello';
+console.log(students[1]);
+students.push('student4');
+console.log(students);
+//in js we can do whatever we want to do
+
+let students2 = [1,'121',{name:'vihaan'},true];
+students2.push(921.89);
+console.log(students2);
+
+console.log(students2.indexOf('1'));
+console.log(students2.indexOf(true));
+students.pop();
+
+students.reverse();
+

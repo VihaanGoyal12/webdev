b8d07859511417403b3f878ddd002a3231df1dda
VihaanGoyal12 <102225589+VihaanGoyal12@users.noreply.github.com>
2026-08-25 23:25:34 +0530

    more with forEach and map

--- javascript/lecture8/index.js
+++ javascript/lecture8/index.js
@@ -1,10 +1,23 @@
 let arr = [12,23,31,98,31,43];
-let sum = 0;
-function print(n){
-    console.log(n);
-    sum = sum+n;
-    return sum;
-}
-arr.forEach(print);
-console.log(sum);
+// let sum = 0;
+// function print(n){
+//     console.log(n);
+//     sum = sum+n;
+//     return sum;
+// }
+// arr.forEach(print);
+// console.log(sum);
 //for each is a high order func
+
+function double(n){
+    return n*2;
+}
+// let newarr = [];
+// arr.forEach((num) =>newarr.push(num*2));
+// console.log(newarr);
+// let newarr = arr.forEach(double);
+// console.log(newarr);
+//gives nothing
+
+let newarr = arr.map(double);
+console.log(newarr);

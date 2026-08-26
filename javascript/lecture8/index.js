7e901c7675675142bf2afdae4013e158195cc41d
VihaanGoyal12 <102225589+VihaanGoyal12@users.noreply.github.com>
2026-08-26 23:14:46 +0530

    functions in arr in js

--- javascript/lecture8/index.js
+++ javascript/lecture8/index.js
@@ -1,4 +1,4 @@
-let arr = [12,23,31,98,31,43];
+let arr = [12,234,4,31,98,31,43];
 // let sum = 0;
 // function print(n){
 //     console.log(n);
@@ -9,9 +9,9 @@ let arr = [12,23,31,98,31,43];
 // console.log(sum);
 //for each is a high order func
 
-function double(n){
-    return n*2;
-}
+// function double(n){
+//     return n*2;
+// }
 // let newarr = [];
 // arr.forEach((num) =>newarr.push(num*2));
 // console.log(newarr);
@@ -19,5 +19,27 @@ function double(n){
 // console.log(newarr);
 //gives nothing
 
-let newarr = arr.map(double);
+// let newarr = arr.map(double);
+// console.log(newarr);
+
+// let ans = arr.find(num =>num === 4);
+// console.log(ans);
+
+// let ans = arr.findIndex(num =>num === 4);
+// console.log(ans);
+
+// let ans = arr.includes(num =>num === 4);
+// console.log(ans);
+
+
+// let newarr = arr.filter(num => num%2 == 0);
+// console.log(newarr);
+
+//0 based indexing, excluding last element
+// let newarr = arr.slice(1,5);
+
+
+//eats elements in the given range
+let newarr = arr.splice(1,5);
 console.log(newarr);
+console.log(arr);
